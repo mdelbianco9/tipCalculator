bd11294723b665aa27d5f9c452564fd9d5e85f6e
Marcus Del Bianco <mdelbianco9@gmail.com>
2019-01-30 00:19:57 -0800

    Updated App UI. Added background-gradient and color scheme.

---
+++ tipCalculator/settingsViewControlerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  settingsViewControlerViewController.swift
+//  tipCalculator
+//
+//  Created by Marcus Del Bianco on 1/29/19.
+//  Copyright © 2019 Marcus Del Bianco. All rights reserved.
+//
+
+import UIKit
+
+class settingsViewControlerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
